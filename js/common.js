4b4cd0d2104db721d1efa6469a7dcf0164f04ea8
Kiheum <gentlekiheum@naver.com>
2018-05-10 17:15:09 +0900

    header gnb 통합

--- js/common.js
+++ js/common.js
@@ -1,17 +1,17 @@
 /* sitemap start */
 var sitemap = function() {
-	var btnStm = $('.btn_stm'),
+	var btnStm = $('.btn_gnb'),
 		stmDimm = $('.m_gnb_dimm');
 
 	btnStm.click(function() {
-		$('.m_nav').animate({
+		$('.gnb_nav').animate({
 			'left' : 0
 		});
 		stmDimm.show();
 	});
 
-	$('.btn_stm_close, .m_gnb_dimm').click(function() {
-		$('.m_nav').animate({
+	$('.btn_gnb_close, .m_gnb_dimm').click(function() {
+		$('.gnb_nav').animate({
 			'left' : '-320px'
 		}, function() {
 			stmDimm.hide();
